b7a7ebb801f513f1c5ae8c8e99472727b42b4cc1
HP Virtus <v.hormazabal03@ufromail.cl>
2025-08-29 10:13:25 -0400

    Metodo pedir frase,encriptar y desencriptar listos, ahora tengo que hacer menu y poner frase como parametro

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,39 @@ public class Main {
     public static void main(String[] args) {
 
     }
+    }
     public boolean verificar_reves_derecho(String[] frase){
         String frase_verificar = pedirFrase().toLowerCase();
-
     };
-    public int contarVocales(String[] frase){
-        String fraseContar = pedirFrase().toLowerCase();
-        for
+    public int contarVocales(){
+        String fraseParaContar = pedirFrase();
+        char[] fraseComoArray = fraseParaContar.toCharArray();
+        int contador = 0;
+        for(char i :)
     };
     public String encriptar(){
-
-    };
+    String fraseEncriptar = pedirFrase();
+    String fraseEncriptadaA = fraseEncriptar.replace("a","@");
+    String fraseEncriptadaE = fraseEncriptadaA.replace("e","&");
+    String fraseEncriptadaI = fraseEncriptadaE.replace("i","!");
+    String fraseEncriptadaO = fraseEncriptadaI.replace("o","*");
+    String fraseEncriptadafinal = fraseEncriptadaO.replace("u","#");
+    return fraseEncriptadafinal;
+};
     public String desencriptar(){
-
+        String fraseEncriptada = encriptar();
+        String fraseDesencriptadaA = fraseEncriptada.replace("@","a");
+        String fraseDesencriptadaE = fraseDesencriptadaA.replace("&","e");
+        String fraseDesencriptadaI = fraseDesencriptadaE.replace("!","i");
+        String fraseDesencriptadaO = fraseDesencriptadaI.replace("*","o");
+        String fraseDesencriptadafinal = fraseDesencriptadaO.replace("#","u");
+        return fraseDesencriptadafinal;
     }
     public String pedirFrase(){
         Scanner input = new Scanner(System.in);
         System.out.println("Escribe una frase: ");
         String frase = input.nextLine();
-        return frase;
+        String fraseSinMayuscula = frase.toLowerCase();
+        return fraseSinMayuscula;
     }
 }
