36154d7079a71da0c0e10d27f27ed6c560901e87
HP Virtus <v.hormazabal03@ufromail.cl>
2025-08-29 09:17:13 -0400

    Primer avance holiiii

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public boolean verificar_reves_derecho(String[] frase){
+        String frase_verificar = pedirFrase().toLowerCase();
+
+    };
+    public int contarVocales(String[] frase){
+        String fraseContar = pedirFrase().toLowerCase();
+        for
+    };
+    public String encriptar(){
+
+    };
+    public String desencriptar(){
+
+    }
+    public String pedirFrase(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Escribe una frase: ");
+        String frase = input.nextLine();
+        return frase;
+    }
+}
